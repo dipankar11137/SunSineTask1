dd6b822a53de57d9ac181803f5b6c402a25f3611
Dipankar <dipankar15-11137@diu.edu.bd>
2022-08-20 01:33:09 +0600

    Add header

--- src/Components/Pages/Home/Header.js
+++ src/Components/Pages/Home/Header.js
@@ -3,7 +3,9 @@ import React from "react";
 const Header = () => {
   return (
     <div>
-      <h1>Header</h1>
+      <h1 className="text-center fw-bold text-uppercase shadow p-2">
+        Welcome To My Website
+      </h1>
     </div>
   );
 };
--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Header from "./Header";
 
 const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
+      <Header />
     </div>
   );
 };
